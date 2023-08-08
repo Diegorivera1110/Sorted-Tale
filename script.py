5659ec5c09604c80a3d743352558b55d798cbb7d
diego rivera <diegorivera1110@gmail.com>
2023-08-08 16:33:04 -0700

    added sorting method by author

--- script.py
+++ script.py
@@ -13,11 +13,17 @@ bookshelf = utils.load_books('books_small.csv')
 book['author_lower'] = book['author'].lower()
 
 def by_title_ascending(book_a, book_b):
-  return book_a['title_lower'] >
-book_b['title_lower']
+  return book_a['title_lower'] > book_b['title_lower']
 
 sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
 
 for book in sort_1:
   print(book['title'])
+
+def by_author_ascending(book_a, book_b):
+  return book_a['author_lower'] > book_b['author_lower']
+  
+bookshelf_v1 = bookshelf.copy()
+
+sort_2 = sorts.bubble_sort(bookshelf_v1, by_author_ascending)
   
