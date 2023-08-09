cfe68fc6709cfdbd88d6cd5a111e101cec0ba73a
diego rivera <diegorivera1110@gmail.com>
2023-08-09 16:04:09 -0700

    updated sorting method by Author and printing funcitons

--- script.py
+++ script.py
@@ -10,15 +10,23 @@ bookshelf_v2 = bookshelf.copy()
 def by_title_ascending(book_a, book_b):
   return book_a['title_lower'] > book_b['title_lower']
 
-sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
-
-for book in sort_1:
-  print(book['title'])
-
+# Sort by Author method
 def by_author_ascending(book_a, book_b):
   return book_a['author_lower'] > book_b['author_lower']
-  
-bookshelf_v1 = bookshelf.copy()
 
+sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
 sort_2 = sorts.bubble_sort(bookshelf_v1, by_author_ascending)
-  
+
+for book in sort_1:
+  print(book['title_lower'])
+
+for book in sort_2:
+  print(book['author_lower'])
+
+  sorts.quicksort(bookshelf_v2, 0, len(bookshelf_v2) - 1, by_author_ascending)
+
+# Sort by Number of Characters in Book & Author's name
+def by_total_length(book_a, book_b):
+  return len(book_a['author_lower']) + len(book_a['title_lower']) > len(book_b['author_lower']) + len(book_b['title_lower'])
+
+long_bookshelf = utils.load_books('books_large.csv')
